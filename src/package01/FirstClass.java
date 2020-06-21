4b2908d6e61c4155bcfe21012bbff505f7dd7b45
AydinC <aydincellek86@gmail.com>
2020-06-21 22:33:34 +0300

    feature branch ten ilk commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -4,8 +4,8 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("ilk commit from local repository");
-		System.out.println("ilk commit from local repository");
-	
+		System.out.println("ikinci commit from local repository");
+	System.out.println("yeni branch olusturdum adi feature");
 	}
 
 }
