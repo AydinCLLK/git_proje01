b9fa09632487859a03a51e85b9f5dc7b626574a6
AydinC <aydincellek86@gmail.com>
2020-06-21 22:17:22 +0300

    ikinci commit local repositoryden

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -4,6 +4,8 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("ilk commit from local repository");
+		System.out.println("ilk commit from local repository");
+	
 	}
 
 }
